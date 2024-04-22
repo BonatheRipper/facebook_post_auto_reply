80d6b2c345bac055f9b5b4ab07425b476c162303
Izeogu Maduabuchi <53067387+BonatheRipper@users.noreply.github.com>
2024-04-22 03:20:10 +0100

    added comments counter fn, ClickwithTextFunction

--- app.js
+++ app.js
@@ -1,13 +1,53 @@
+require("fix-esm").register();
 const puppeteer = require("puppeteer");
 const fs = require("fs");
 const config = require("./config.json");
 const cookies = require("./cookies.json");
+const { currentLinks, homePage, LoginPage } = require("./links.js");
+const { scrollPageToBottom } = require("puppeteer-autoscroll-down");
 const sleep = (ms) => new Promise((res) => setTimeout(res, ms));
-(async () => {
+const toLink = async (page, link) => {
+  await page.goto(link, {
+    waitUntil: "networkidle2",
+  });
+};
+// Function to scroll to the bottom of the page
+const scrollToBottomOfPage = async (page) => {
+  await scrollPageToBottom(page, {
+    size: 500,
+    delay: 250,
+  });
+};
+
+const clickElementWithText = async (page, tagName, searchText) => {
+  const matchedTexts = await page.evaluate(
+    (tagName, searchText) => {
+      const matchedTexts = [];
+      const elements = document.querySelectorAll(tagName);
+      elements.forEach((element) => {
+        if (element.innerText == searchText) {
+          element.click();
+          matchedTexts.push(searchText);
+        }
+      });
+      return matchedTexts;
+    },
+    tagName,
+    searchText
+  );
+
+  if (matchedTexts.length > 0) {
+    await sleep(1000);
+    await scrollToBottomOfPage(page);
+  }
+};
+
+// Defining a function to scroll to the bottom of the page
+
+const Start = async () => {
   // Launch the browser and open a new blank page
   const browser = await puppeteer.launch({
     headless: false,
-    args: ["--start-maximized"],
     defaultViewport: null,
   });
   const page = await browser.newPage();
@@ -15,12 +55,23 @@ const sleep = (ms) => new Promise((res) => setTimeout(res, ms));
   //   Check if we previouly have a saved session
   if (Object.keys(cookies).length) {
     // set the saved cookies in puppeteer broser page
-    await page.setCookies(...cookies);
-    // goto facebook
-    await page.goto("https://www.facebook.com/", { waitUntil: "networkidle2" });
+    await page.setCookie(...cookies);
+    // goto new  facebook  url
+
+    // await toLink(page, homePage);
+    // await sleep(2000);
+    await toLink(page, currentLinks[0]);
+    await scrollToBottomOfPage(page); // Call the function here
+
+    await sleep(5000);
+    await clickElementWithText(page, "span", "View more comments");
+
+    await sleep(120000);
+
+    await browser.close();
   } else {
     // goto login page
-    await page.goto("https://www.facebook.com/login", {
+    await page.goto(LoginPage, {
       waitUntil: "networkidle2",
     });
     // write in the username and password
@@ -33,13 +84,15 @@ const sleep = (ms) => new Promise((res) => setTimeout(res, ms));
 
     //wait for navigation to finsh
     await page.waitForNavigation({ waitUntil: "networkidle0" });
-    await sleep(60000);
+    await sleep(15000);
     // checked if logged in
     try {
       const exists = await page.$("h1::-p-text(Friends)");
 
       if (exists) {
         console.log("YES IT EXIST");
+      } else {
+        console.log("IT DOES NOT EXIST");
       }
     } catch (error) {
       console.log({ 1: "Failed to login", 2: error });
@@ -52,4 +105,6 @@ const sleep = (ms) => new Promise((res) => setTimeout(res, ms));
     fs.writeFileSync("./cookies.json", JSON.stringify(currentCookies));
   }
   debugger;
-})();
+};
+Start();
+// console.log(currentLinks[0]);
---
+++ links.js
@@ -0,0 +1,6 @@
+const currentLinks = [
+  "https://www.facebook.com/share/FYZvyK2EsU7EW3QL/?mibextid=WC7FNe",
+];
+const homePage = "https://www.facebook.com";
+const LoginPage = "https://www.facebook.com/login";
+module.exports = { currentLinks, homePage, LoginPage };
